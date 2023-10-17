2ea77450b1b065058c0020fc1e75e6c8d5f6d988
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2023-10-17 16:21:13 -0300

    function of deleting a book done

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@ const addBook = document.querySelector(".container__add");
 const containerForm = document.querySelector(".container__form");
 const form = document.querySelector(".form");
 const containerBooks = document.querySelector(".container__books");
-const templateBook = document.getElementById('templateBook');
+const templateBook = document.getElementById("templateBook");
 const fragment = document.createDocumentFragment();
-let date = new Date();
 
 const bookList = [
   {
@@ -44,54 +43,61 @@ function showBooks(list) {
   list.forEach((book) => {
     const readStatus = book.read ? "Read" : "Not Read";
 
-    const clone = templateBook.content.cloneNode(true)
+    const clone = templateBook.content.cloneNode(true);
 
-    clone.querySelector('.container__book').setAttribute('data-id', book.id)
+    clone.querySelector(".container__book").setAttribute("data-id", book.id);
 
-    clone.querySelector('.container__title').textContent = book.title;
-    clone.querySelector('.container__author').textContent = book.author;
-    clone.querySelector('.container__pages').textContent = `${book.pages} pages`;
-    let read = clone.querySelector('.container__read')
+    clone.querySelector(".container__title").textContent = book.title;
+    clone.querySelector(".container__author").textContent = book.author;
+    clone.querySelector(
+      ".container__pages"
+    ).textContent = `${book.pages} pages`;
+    let read = clone.querySelector(".container__read");
     read.textContent = readStatus;
 
-    if (read.textContent === 'Read') {
-      read.classList.add('readYes')
+    if (read.textContent === "Read") {
+      read.classList.add("readYes");
     } else {
-      read.classList.add('readNo')
+      read.classList.add("readNo");
     }
 
-    clone.querySelector('.container__read').addEventListener('click', (e) => {
-      console.log('si');
-      console.log(book.read);
-      let parent = e.target.parentElement.parentElement.getAttribute('data-id');
+    clone.querySelector(".container__read").addEventListener("click", (e) => {
+      let parent = e.target.parentElement.parentElement.getAttribute("data-id");
 
-      bookList.forEach(book => {
+      bookList.forEach((book) => {
         if (book.id === parseInt(parent)) {
           if (book.read) {
-            read.classList.remove('readYes')
-            read.classList.add('readNo')
-            read.textContent = 'Not Read'
-            book.read = false
-            console.log(bookList);
+            read.classList.remove("readYes");
+            read.classList.add("readNo");
+            read.textContent = "Not Read";
+            book.read = false;
           } else {
-            read.classList.remove('readNo')
-            read.classList.add('readYes')
-            read.textContent = 'Read'
-            book.read = true
-            console.log(bookList);
+            read.classList.remove("readNo");
+            read.classList.add("readYes");
+            read.textContent = "Read";
+            book.read = true;
           }
         }
       });
+    });
 
-    })
-    clone.querySelector('.container__delete').addEventListener('click', () => {
-      console.log('holas');
-    })
+    clone.querySelector(".container__delete").addEventListener("click", (e) => {
+      let parent = e.target.parentElement.parentElement.getAttribute("data-id");
 
-    fragment.appendChild(clone)
+      bookList.forEach((book, index) => {
+        if (book.id === parseInt(parent)) {
+          console.log("si");
+          bookList.splice(index, 1);
+        }
+      });
+
+      showBooks(bookList);
+    });
+
+    fragment.appendChild(clone);
   });
 
-  containerBooks.appendChild(fragment)
+  containerBooks.appendChild(fragment);
 }
 
 addBook.addEventListener("click", () => {
